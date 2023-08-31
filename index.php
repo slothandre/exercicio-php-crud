82772141dd1f9c76be5a8cb7421be1000d897c87
André Marques <andre.m.d.abreu@gmail.com>
2023-08-31 10:41:25 -0300

    Pequena alteração no HTML

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício CRUD com PHP e MySQL</title>
-<link href="css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício CRUD com PHP e MySQL</title>
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <h1>Exercício - CRUD com PHP e MySQL</h1>
-    <hr>
-    <h2>Gerenciamento de alunos, notas, médias e aprovação/reprovação</h2>
-    <hr>
-    <ul>
-        <li><a href="visualizar.php">Visualizar Alunos</a></li>
-        <li><a href="inserir.php">Inserir novo aluno</a></li>
-    </ul>
-</div>
+    <div class="container">
+        <header>
+            <h1>Exercício - CRUD com PHP e MySQL</h1>
+            <hr>
+        </header>
+        <main>
+            <h2>Gerenciamento de alunos, notas, médias e aprovação/reprovação</h2>
+            <hr>
+            <ul>
+                <li><a href="visualizar.php">Visualizar Alunos</a></li>
+                <li><a href="inserir.php">Inserir novo aluno</a></li>
+            </ul>
+        </main>
+    </div>
 
 </body>
 </html>
