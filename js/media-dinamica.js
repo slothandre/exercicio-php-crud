fe90b117b7268475c2e349524efd7c963f6b8343
André Marques <andre.m.d.abreu@gmail.com>
2023-09-01 11:54:16 -0300

    Refatoração na mudança de cores do input de atualizar

--- js/media-dinamica.js
+++ js/media-dinamica.js
@@ -3,18 +3,41 @@ const nota2 = document.querySelector("#segunda");
 const inputMedia = document.querySelector("#media");
 const inputSituacao = document.querySelector("#situacao");
 
-function notaSituacaoDinamica(){
+function notaDinamica(){
     let media = ((parseFloat(nota1.value) + parseFloat(nota2.value)) / 2);
     inputMedia.value = media.toFixed(2);
+};
 
-    if(media >= 7){
+function situacaoDinamica(){
+    if(inputMedia.value >= 7){
         inputSituacao.value = "Aprovado";
-    } else if(media >= 5){
+    } else if(inputMedia.value >= 5){
         inputSituacao.value = "Recuperação";
     } else {
         inputSituacao.value = "Reprovado";
-    } 
+    }
 };
 
-nota1.addEventListener("input", notaSituacaoDinamica);
-nota2.addEventListener("input", notaSituacaoDinamica);
+function backgroundClass(element, success, warning, danger){
+    element.classList.toggle("text-bg-success", success);
+    element.classList.toggle("text-bg-warning", warning);
+    element.classList.toggle("text-bg-danger", danger);
+}
+
+function backgroundDinamico(){
+    const mediaValue = inputMedia.value;
+
+    backgroundClass(inputMedia, mediaValue >= 7, mediaValue >= 5 && mediaValue < 7, mediaValue < 5);
+    backgroundClass(inputSituacao, mediaValue >= 7, mediaValue >= 5 && mediaValue < 7, mediaValue < 5);
+}
+
+nota1.addEventListener("input", () => {
+    notaDinamica();
+    situacaoDinamica();
+    backgroundDinamico();
+})
+nota2.addEventListener("input", () => {
+    notaDinamica();
+    situacaoDinamica();
+    backgroundDinamico();
+})
